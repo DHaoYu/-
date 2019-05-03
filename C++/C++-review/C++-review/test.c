1fa1b31129221c2c16fbe864361a1e6ca21c5baa
DHaoYu <178070667@qq.com>
2019-05-03 18:12:47 +0800

    compare-c-c++

---
+++ C++/C++-review/C++-review/test.c
@@ -0,0 +1,11 @@
+//#define _CRT_SECURE_NO_WARNNINGS 1
+//#include<stdio.h>
+//
+//int main()
+//{
+//	const int a = 10;
+//	int* pa = (int*)&a;
+//	*pa = 100;
+//	printf("%d", a);  //a = 100;
+//	return 0;
+//}
