3ea114926712d71100d4445e3b8cdfcc64e6c501
DHaoYu <178070667@qq.com>
2019-05-12 19:31:26 +0800

    message

--- C++/C++-review/C++-review/test.c
+++ C++/C++-review/C++-review/test.c
@@ -9,3 +9,26 @@
 //	printf("%d", a);  //a = 100;
 //	return 0;
 //}
+
+
+//#include<malloc.h>
+//#include<stdio.h>
+//
+//int* Test()
+//{
+//	int* arr = (int*)malloc(10);
+//	for (int i = 0; i < 10; i++)
+//	{
+//		*(arr++) = i;
+//	}
+//	return arr;
+//}
+//
+//
+//int main()
+//{
+//	int* arr = Test();
+//	for (int i = 0; i < 10; i++)
+//		printf("%d ", *(arr + i));
+//	return 0;
+//}
