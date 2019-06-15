c6e95264102dd007e956666a42968403fde2fe0e
DHaoYu <178070667@qq.com>
2019-06-15 18:07:41 +0800

    day_1

--- examination/exam_week4/exam_week4/day_1.cpp
+++ examination/exam_week4/exam_week4/day_1.cpp
@@ -1,99 +1,66 @@
+#define _CRT_SECURE_NO_WARNNINGS 1
 #define _CRT_SECURE_NO_WARNINGS 1
-#if 0
+
 #include<iostream>
 using namespace std;
 
+#if 0
 int main()
 {
-	int y, m, d;
-	int sum;
-	bool leap = false;
-	cin >> y >> m >> d;
-	if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0))
-		leap = true;
-	switch (m){
-	case 1:sum = 0; break;
-	case 2:sum = 31; break;
-	case 3:sum = 59; break;
-	case 4: sum = 90; break;
-	case 5:sum = 120; break;
-	case 6:sum = 151; break;
-	case 7:sum = 181; break;
-	case 8:sum = 212; break;
-	case 9:sum = 243; break;
-	case 10:sum = 273; break;
-	case 11:sum = 304; break;
-	case 12:sum = 334; break;
-	default:
-		cout << -1 << endl;
-		return 0;
-	}
-	if (d <= 0 || d >= 31)
+	int n;
+	while (cin >> n)
 	{
-		cout << -1 << endl;
-		return 0;
+		if (n == 0)
+			break;
+		n += (n / 2);
+		cout << n / 3 << endl;
 	}
-
-	if (m == 2)
-	{
-		if (leap==false)
-		if (d == 29)
-			cout << -1 << endl;
-		return 0;
-	}
-
-	sum += d;
-	if (leap && m>2)
-		sum++;
-	cout << sum << endl;
-	return 0;
+	return  0;
 }
 #endif
 
+
 #if 0
-#include<iostream>
-#include <stdlib.h>
-using namespace std;
+#include<string>
 
-int n;
-int nums[1000];
-
-int cmp(const void * a, const void * b) {
-	return *(int*)a - *(int*)b;
-}
-
-// 思路：DFS生成全组合，同时注意剪枝、避免重复组合
-int findall(int nums[], int index, long sum, long multi) {
-	int count = 0;
-	for (int i = index; i<n; i++) {
-		sum += nums[i];
-		multi *= nums[i];
-		if (sum > multi)
-			count += 1 + findall(nums, i + 1, sum, multi);
-		else if (nums[i] == 1)
-			count += findall(nums, i + 1, sum, multi);
-		else
-			break;
-		sum -= nums[i];
-		multi /= nums[i];
-		// 跳过相等的元素，避免重复组合
-		while (i<n - 1 && nums[i] == nums[i + 1])
-			i++;
+void Longsubstring(string& s1, string& s2)
+{
+	size_t count = 0;
+	size_t max = 0;
+	string str;
+	if (s1.size()>s2.size())
+		swap(s1, s2);
+	for (size_t i = 0; i < s1.size(); i++)
+	{
+		for (size_t j = 0; j < s2.size(); j++)
+		{
+			size_t k = i;
+			while (s2[j] == s1[k] && k<s1.size() && j<s2.size())
+			{
+				count++;
+				k++;
+				j++;
+			}
+			if (count>max)
+			{
+				str = s1.substr(i, count);
+				max = count;
+			}
+			count = 0;
+		}
 	}
-	return count;
+	cout << str << endl;
 }
 
 int main()
 {
-	while (cin >> n) {
-		for (int i = 0; i<n; i++)
-			cin >> nums[i];
-
-		// 从小到大排序
-		qsort(nums, n, sizeof(int), cmp);
-		cout << findall(nums, 0, 0, 1) << endl;
+	string s1, s2;
+	while (getline(cin, s1))
+	{
+		getline(cin, s2);
+		Longsubstring(s1, s2);
 	}
-	//太难了，一点思路都没有！！！
+
 	return 0;
 }
 #endif
