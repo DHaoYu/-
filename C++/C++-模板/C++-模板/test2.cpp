dbc7b9d31b9ea7ed0cabbf3ee6eb51c8fe4b3e15
DHaoYu <178070667@qq.com>
2019-07-05 21:32:04 +0800

    test2

--- C++/C++-模板/C++-模板/test2.cpp
+++ C++/C++-模板/C++-模板/test2.cpp
@@ -98,3 +98,47 @@ int main()
 	return 0;
 }
 #endif
+
+#if 0
+template<class T>
+class Printer
+{
+public:
+	Printer(const T& t)
+	:_t(t)
+	{}
+
+	void print()
+	{
+		cout << _t << endl;
+	}
+
+private:
+	T _t;
+};
+
+class empty
+{};
+
+int main()
+{
+	empty e;
+	Printer<empty> p(e);//实例化empty类型的p，但是empty中没有重载打印函数，所以
+	p.print();//在调用print函数时会出错，但是如果不调用就不会报错。
+	//为了节省资源，类模板实例化并不是每个成员函数都生成代码，而是使用到那个成员函数实例化那个成员函数
+	return 0;
+}
+#endif
+
+#include<string>
+//类模板别名
+//模板的声明只能在全局，命名空间或类中使用***
+template<class T> using WithNum = pair<T, int>;//定义了一个pair的偏特化
+WithNum<string> strs;//定义了一个pair<string,int> 类型对象strs
+int main()
+{
+	typedef pair<int, int> PairInt;//1 定义了一个别名
+
+	PairInt p;//定义一个pair<int,int> 对象 p
+	return 0;
+}
