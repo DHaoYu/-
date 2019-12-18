0cf125c0f20d9689bbe7c67462c62f541ec744c7
DHaoYu <178070667@qq.com>
2019-12-18 21:57:02 +0800

    test8

--- C++/C++-继承/C++-继承/test8.cpp
+++ C++/C++-继承/C++-继承/test8.cpp
@@ -42,6 +42,8 @@ int main()
 }
 #endif
 
+
+#if 0
 class A //價濬
 {
 protected:
@@ -75,3 +77,74 @@ int main()
 	D d;
 	d.Print();
 }
+#endif
+
+#if 0
+class A
+{
+protected:
+	int _a;
+	int a;
+};
+
+class B : public A
+{
+protected:
+	int _b;
+};
+
+class C : public A
+{
+protected:
+	int _C;
+};
+
+class D : public B, public C
+{
+protected:
+	int _d;
+};
+
+int main()
+{
+	cout << sizeof(A) << endl;
+	cout << sizeof(B) << endl;
+	cout << sizeof(C) << endl;
+	cout << sizeof(D) << endl;
+}
+#endif
+
+#if 0
+class A //價濬
+{
+protected:
+	int _a1 = 1;
+	int _a2 = 10;
+};
+
+class B : virtual public A //B樟創A
+{
+protected:
+	int _b = 2;
+};
+
+class C : virtual public  A //C樟創A
+{
+protected:
+	int _c = 3;
+};
+
+class D : public B, public C //D樟創B﹜C
+{
+protected:
+	int _d = 4;
+};
+
+int main()
+{
+	cout << sizeof(A) << endl; //8
+	cout << sizeof(B) << endl; //16
+	cout << sizeof(C) << endl; //16
+	cout << sizeof(D) << endl; //28
+}
+#endif
