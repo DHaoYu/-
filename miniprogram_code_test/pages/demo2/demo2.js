8ba5cfc73023ab319c6d3892342b9626c8248e27
DHaoYu <178070667@qq.com>
2021-04-07 00:06:37 +0800

    wx_miniprogram_demo

---
+++ miniprogram_code_test/pages/demo2/demo2.js
@@ -0,0 +1,71 @@
+// pages/demo2/demo2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    message: "学习小程序开发",
+    isTrue: true,
+    listArr: ["数据结构", "软件测试", "算法设计"],
+    human: {name: "董皓宇", age: 20, sex: "男"},
+    data: 4,
+    length: 10
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
